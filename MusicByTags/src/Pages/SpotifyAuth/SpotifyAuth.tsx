0c0cc14496e2056f5e4e42bb6d852e80686db76d
Robert <rdshakura@gmail.com>
2024-09-29 20:13:09 +0300

    add auth

--- MusicByTags/src/Pages/SpotifyAuth/SpotifyAuth.tsx
+++ MusicByTags/src/Pages/SpotifyAuth/SpotifyAuth.tsx
@@ -1,45 +1,47 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { TOKEN } from "../../workWithAPI/getTOKEN";
+import { REFRESH_TOKEN, TOKEN } from "../../workWithAPI/getTOKEN";
+import { useNavigate } from "react-router-dom";
 
 const clientId = "342af3e686ee41a9ae548be73e9b4f74";
-//const redirectUri = "https://silver-bonbon-26555e.netlify.app/search/";
-const redirectUri = "http://localhost:5173/search/"; // Например, 'http://localhost:3000'
-const scopes = "user-read-private user-read-email"; // Scopes, которые нужны для API запросов
-const spotifyAuthUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+const secret = "b47aec4e0e704aa98d2cf8044a075b15";
+const scopes = "user-read-private user-read-email";
+
+const getAuthUrl = () => {
+    const currentUrl = window.location.href;
+    const spotifyAuthUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(currentUrl)}`;
+    return spotifyAuthUrl;
+};
 
 const SpotifyAuthComponent = () => {
   const [accessToken, setAccessToken] = useState<string | null>(null);
+  const naviaget = useNavigate();
 
-  // Проверка наличия токена в localStorage и обработка кода из URL
   useEffect(() => {
     const tokenFromStorage = localStorage.getItem(TOKEN);
     const codeFromUrl = new URLSearchParams(window.location.search).get("code");
 
-    // Если токена нет, но есть код из URL (после авторизации)
     if (!tokenFromStorage && codeFromUrl) {
-      // Получаем токен по коду
+      console.log("HERE");
       getTokenFromCode(codeFromUrl);
     } 
-    // Если токен отсутствует, перенаправляем на авторизацию
     else if (!tokenFromStorage) {
-      window.location.href = spotifyAuthUrl;
+      window.location.href = getAuthUrl();
     } else {
-      // Если токен есть, используем его
       setAccessToken(tokenFromStorage);
     }
   }, []);
 
-  // Функция для получения токена по коду
   const getTokenFromCode = async (code: string) => {
     try {
+      const currentUrl = window.location.href;
       const response = await axios.post("https://accounts.spotify.com/api/token", null, {
         params: {
           grant_type: "authorization_code",
           code: code,
-          redirect_uri: redirectUri,
+          redirect_uri: currentUrl,
           client_id: clientId,
-          client_secret: "YOUR_CLIENT_SECRET"
+          client_secret: secret
         },
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
@@ -48,42 +50,28 @@ const SpotifyAuthComponent = () => {
 
       const { access_token, refresh_token } = response.data;
 
-      // Сохраняем токен в localStorage
-      localStorage.setItem("access_token", access_token);
-      localStorage.setItem("refresh_token", refresh_token);
+      localStorage.setItem(TOKEN, access_token);
+      localStorage.setItem(REFRESH_TOKEN, refresh_token);
 
-      // Устанавливаем токен для дальнейшего использования
       setAccessToken(access_token);
 
-      // Убираем код из URL, чтобы не мешал при следующем запросе
       window.history.replaceState({}, document.title, window.location.pathname);
     } catch (error) {
       console.error("Error fetching access token:", error);
     }
   };
 
-  // Пример использования токена для API запросов
   const fetchUserData = async () => {
     if (!accessToken) return;
 
-    try {
-      const response = await axios.get("https://api.spotify.com/v1/me", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      });
-
-      console.log("User data:", response.data);
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-    }
+    naviaget("/search");
   };
 
   return (
     <div>
       <h1>Spotify Authorization</h1>
       {accessToken ? (
-        <button onClick={fetchUserData}>Get User Data</button>
+        <button onClick={fetchUserData}>Go to app</button>
       ) : (
         <p>Redirecting to Spotify for authorization...</p>
       )}
--- MusicByTags/src/Pages/TotalSearch/TotalSearch.tsx
+++ MusicByTags/src/Pages/TotalSearch/TotalSearch.tsx
@@ -10,6 +10,7 @@ import { Track } from "../../interfaces/Track.interface";
 import { AxiosError } from "axios";
 import { searchAPI } from "../../workWithAPI/searchAPI";
 import { PlayerActions } from "../../Store/CurrentTrackStateSlices/playerManager.slice";
+// import { TOKEN } from "../../workWithAPI/getTOKEN";
 
 // object - current search list
 function TotalSearch() {
